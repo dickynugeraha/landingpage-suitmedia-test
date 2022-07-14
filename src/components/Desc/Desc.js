4a72952360896ce456405ad95216d74cbec0b55c
Dicky Nugeraha <dicky.dian1@gmail.com>
2022-07-14 15:44:38 +0700

    [FINAL]

--- src/components/Desc/Desc.js
+++ src/components/Desc/Desc.js
@@ -2,7 +2,14 @@ import React from "react";
 import classes from "./Desc.module.css";
 
 const Desc = () => {
-  return <div className={classes.desc}></div>;
+  return (
+    <div className={classes.desc}>
+      <div>
+        THIS IS A PLACE WHERE TECHNOLOGY & CREATIVITY FUSED INTO DIGITAL
+        CHEMISTRY
+      </div>
+    </div>
+  );
 };
 
 export default Desc;
